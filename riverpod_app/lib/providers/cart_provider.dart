1f205a1a5b53cbb908e18f5ec7335bb5e956a554
Vung Nguyen <vungnguyenn1001@gmail.com>
2024-06-24 17:35:18 +0700

    Generated Notifier Provider

--- riverpod_app/lib/providers/cart_provider.dart
+++ riverpod_app/lib/providers/cart_provider.dart
@@ -1,18 +1,14 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:riverpod_app/models/product.dart';
 
-class CartNotifier extends Notifier<Set<Product>> {
+part 'cart_provider.g.dart';
+
+@riverpod
+class CartNotifier extends _$CartNotifier {
   // inital value
   @override
   Set<Product> build() {
-    return {
-      const Product(
-        id: '4',
-        title: 'Red Backpack',
-        price: 14,
-        image: 'assets/products/backpack.png',
-      ),
-    };
+    return const {};
   }
 
   // method update state
@@ -28,7 +24,3 @@ class CartNotifier extends Notifier<Set<Product>> {
     }
   }
 }
-
-final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(() {
-  return CartNotifier();
-});
